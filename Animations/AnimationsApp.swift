3cae6fc8817f0ebbf36eaa76365947c8c5e11b5f
andruw-sorensen <andruw.sorensen@gmail.com>
2024-02-05 14:59:52 -0700

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Andruw Sorensen on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
